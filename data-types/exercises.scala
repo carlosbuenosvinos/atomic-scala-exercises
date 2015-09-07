0baa1ebda8936f745fb0b12ef146043ac18a9b15
Carlos Buenosvinos <hi@carlos.io>
2015-09-07 11:21:22 +0200

    Values and Data Types solutions

---
+++ data-types/exercises.scala
@@ -0,0 +1,35 @@
+// 1. Store the value 5 as an Int and print the value.
+val a:Int = 5
+println(a)
+
+// 2. Store (and print) the value “ABC1234” as a String.
+val b:String = "ABC1234"
+println(b)
+
+// 3. Store the value 5.4 as a Double. Print it.
+val c:Double = 5.4
+println(c)
+
+// 4. Store the value true. What type did you use? What did it print?
+val d:Boolean = true
+println(d)
+
+// 5. Store a multiline String. Does it print in multiple lines?
+val e:String = """Hi,
+This is a multiline string.
+Best,
+Carlos
+"""
+println(e)
+
+// 6. What happens if you try to store the String “maybe” in a Boolean?
+// val f:Boolean = "maybe"
+// error: type mismatch;
+
+// 7. What happens if you try to store the number 15.4 in an Int value?
+// val g:Int = 15.4
+// error: type mismatch;
+
+8. What happens if you try to store the number 15 in a Double value? Print it.
+val h:Double = 15
+println(h)
---
+++ values/exercises.scala
@@ -0,0 +1,19 @@
+// 1. Store (and print) the value 17.
+val foo = 17
+println(foo)
+
+// 2. Using the value you just stored (17), try to change it to 20. What happened?
+// foo = 20
+// error: reassignment to val
+
+// 3. Store (and print) the value “ABC1234.”
+val bar = "ABC1234"
+println(bar)
+
+// 4. Using the value you just stored (“ABC1234”), try to change it to “DEF1234.” What happened?
+// bar = "DEF1234"
+// error: reassignment to val
+
+// 5. Store the value 15.56. Print it.
+val buzz = 15.56
+println(15.56)
